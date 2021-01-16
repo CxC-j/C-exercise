03b456cf8baf0e461020832a5de9a73d0944bd2c
master <2279309706@qq.com>
2021-01-16 16:05:49 +0800

    master

--- chapter2.cpp
+++ chapter2.cpp
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(void)
 {
-    int num;
+    int num; //定义 变量 num  
     num = 1;
-    printf("I am a simple");;
+    num=2;
+    printf("I am a simple ");;
     printf("computer.\n");
     printf("My favourite number is %d because it is first .\n",num);
     return 0;
- 
+    
 }
