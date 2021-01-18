100a9d1da098e69602b472098d217eae241aa299
master <2279309706@qq.com>
2021-01-18 15:04:05 +0800

    master

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+    int num=1;
+    /*while (num < 21 )
+    {
+        printf("%4d %6d\n%",num,num*num);
+        num = num + 1;
+    }*/
+    char c;
+
+    scanf("%c",&c);
+    printf("%c",c);
+    printf("jiyulong");
+    return 0;
+    
+}
